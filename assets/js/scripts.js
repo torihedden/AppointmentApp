b05261904939bbbee2184080561f8a75ccf882a0
Byron Wall <byrondevonwall@gmail.com>
2016-02-25 14:12:15 -0500

    Add functional new/edit appt templates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 //code from http://jstricks.com/detect-mobile-devices-javascript-jquery/ --all code should be within this function
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
   // tasks to do if it is a Mobile Device
-  alert("Mobile Detected");
+  console.log("Mobile Detected");
 
 }
 
