c51c59b3041fce2361f020f59ef7375583494201
Byron Wall <byrondevonwall@gmail.com>
2016-02-26 09:51:52 -0500

    Add functioning object/array storage

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,19 +7,24 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
   console.log("Mobile Detected");
 
 }
-var submitClick = 0;
+var apptArray = []
 var apptRetrieve
+var apptInfo
 $(".save-appt-btn").on("click", function(){
-  submitClick += 1;
-  ////this code sets an object into localStorage as a string and brings it back out and parses it as an object.  From http://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage
-  var apptInfo = {title: $(".title-input").val(), street: $(".street-input").val(), city: $(".city-input").val(), date: $(".date-input").val(), time: $(".time-input").val(), id: submitClick};
-  console.log(apptInfo);
-   localStorage.setItem('apptInfo', JSON.stringify(apptInfo));
-   var apptRetrieve = localStorage.getItem('apptInfo');
+apptArray = JSON.parse(localStorage["apptArray"]);
+function pushAppt(appt){
+  apptArray.push(appt);
+}
+  var apptInfo = {title: $(".title-input").val(), street: $(".street-input").val(), city: $(".city-input").val(), date: $(".date-input").val(), time: $(".time-input").val()};
 
-   console.log(apptRetrieve = localStorage.getItem('apptInfo'));
-   console.log('apptRetrieve: ', JSON.parse(apptRetrieve));
+  pushAppt(apptInfo);
+  console.log(apptArray);
+    ////this code sets an object into localStorage as a string and brings it back out and parses it as an object.  From http://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage
+   localStorage.setItem('apptArray', JSON.stringify(apptArray));
+   apptArray = JSON.parse(localStorage["apptArray"]);
+   console.log(apptArray);
 });
+// console.log(apptArray);
 
 
 });//this closes the entire function
