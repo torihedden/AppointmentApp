faa314864be3af2210231f301195c1a8c17fe27f
Byron Wall <byrondevonwall@gmail.com>
2016-02-25 20:28:57 -0500

    clean up JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,7 +8,7 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 
 }
 var submitClick = 0;
-var apptRetrieve 
+var apptRetrieve
 $(".save-appt-btn").on("click", function(){
   submitClick += 1;
   ////this code sets an object into localStorage as a string and brings it back out and parses it as an object.  From http://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage
@@ -22,25 +22,4 @@ $(".save-appt-btn").on("click", function(){
 });
 
 
-// Storage.prototype.setObject = function(key, value) {
-//     this.setItem(key, JSON.stringify(value));
-// }
-//
-// Storage.prototype.getObject = function(key) {
-//     var value = this.getItem(key);
-//     return value && JSON.parse(value);
-// }
-//
-// var testObject1 = { 'one': 1, 'two': 2, 'three': 3 };
-// var testObject2 = { 'one': 12, 'two': 22, 'three': 32 };
-//
-// localStorage.setItem('testObject1', JSON.stringify(testObject1));
-// localStorage.setItem('testObject2', JSON.stringify(testObject2));
-//
-//
-// var retrievedObject1 = localStorage.getItem('testObject1');
-// var retrievedObject2 = localStorage.getItem('testObject2');
-//
-// console.log('retrievedObject: ', JSON.parse(retrievedObject1));
-// console.log('retrievedObject: ', JSON.parse(retrievedObject2));
 });//this closes the entire function
