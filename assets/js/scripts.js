51f7d6715e6accb6a7dd6c8f7ea7bbc15534e51e
Byron Wall <byrondevonwall@gmail.com>
2016-02-26 11:52:25 -0500

    make it so that JS initializes array in local storage

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   'use strict';
-  var apptArray = []
+  var apptArray = [];
   var apptRetrieve
   var apptInfo
+  localStorage.setItem('apptArray', JSON.stringify(apptArray));//add empty array to localStorage
 //code from http://jstricks.com/detect-mobile-devices-javascript-jquery/ --all code should be within this function
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
   // tasks to do if it is a Mobile Device
@@ -14,7 +15,7 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 
     function pushAppt(appt){
     apptArray.push(appt);
-  }//func to push appt info to array
+    }//func to push appt info to array
 
     var apptInfo = {title: $(".title-input").val(), street: $(".street-input").val(), city: $(".city-input").val(), date: $(".date-input").val(), time: $(".time-input").val()};//grab appt info
 
@@ -23,7 +24,16 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
      localStorage.setItem('apptArray', JSON.stringify(apptArray));
      apptArray = JSON.parse(localStorage["apptArray"]);
      console.log(apptArray);
-  });  ////this code made with help from http://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage
+
+  });  ////this code made with help from http://stackoverflow.com/questions/19174525/how-to-store-array-in-localstorage-object-in-html5
+
+  //this section deletes appt objects from the appt array
+  // $(".delete-appt-btn").on("click", function(){
+  //   apptArray = JSON.parse(localStorage["apptArray"]);//bring array out of localStorage
+  //
+  //
+  // });//end of delete func
+    //  localStorage.clear()
 }
 
 
