37e66fd8fb9d06e7924e8b1499a58ea240200a76
Tori Hedden <tori.hedden@gmail.com>
2016-02-26 13:48:11 -0500

    Adds rudimentary weather updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -55,6 +55,18 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
   //
   // });//end of delete func
     //  localStorage.clear()
+
+    var cityName = "Atlanta"
+    var stateName = "GA"
+
+    $.getJSON("http://api.wunderground.com/api/b80f8aa82340bfd9/conditions/q/" + stateName + "/" + cityName + ".json", function(json) {
+      $(".weather-block").html(json.current_observation.weather);
+    });
+
+    <i class="fa fa-sun-o"></i>
+    <i class="fa fa-cloud"></i>
+    <i class="fa fa-umbrella"></i>
+
 }
 
 //some changes...
